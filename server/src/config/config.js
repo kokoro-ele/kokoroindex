5bd1801d5ac8e48d9b1e6fe1d2543c58f46d188d
kokoro <62824035+kokoro-ele@users.noreply.github.com>
2022-08-15 22:36:25 +0800

    fix

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -13,13 +13,13 @@ module.exports = {
   dbConfig: {
     database: "myindex",
     username: "root",
-    password: "Tt881123",
+    password: "",
     host: "localhost",
     port: 3306,
   },
   // 百度翻译配置，自行申请免费版即可（https://fanyi-api.baidu.com/）
   baiduFanYiConfig: {
-    appid: "20220810001301461",
-    key: "NMde9g8VbK_sqtKt_NdT",
+    appid: "",
+    key: "",
   },
 };
