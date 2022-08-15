02933f93c382c6ba7b7a60bf7db62eb4ef80767a
kokoro-ele <kokoro_ele@163.com>
2022-08-15 21:53:56 +0800

    first commit

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -6,20 +6,20 @@ module.exports = {
   redisConfig: {
     host: "localhost",
     port: "6379",
-    password: "123456",
+    password: "",
     db: 2,
   },
   // MySQL 配置
   dbConfig: {
-    database: "yuindex",
+    database: "myindex",
     username: "root",
-    password: "123456",
+    password: "Tt881123",
     host: "localhost",
     port: 3306,
   },
   // 百度翻译配置，自行申请免费版即可（https://fanyi-api.baidu.com/）
   baiduFanYiConfig: {
-    appid: "",
-    key: "",
+    appid: "20220810001301461",
+    key: "NMde9g8VbK_sqtKt_NdT",
   },
 };
