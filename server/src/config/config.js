454f475850d26d47d0f2346b398c81bf996f5559
kokoro-ele <kokoro_ele@163.com>
2022-08-15 23:14:34 +0800

    666

---
+++ server/src/config/config.js
@@ -0,0 +1,25 @@
+/**
+ * 默认配置
+ * @author yupi
+ */
+module.exports = {
+  redisConfig: {
+    host: "localhost",
+    port: "6379",
+    password: "",
+    db: 2,
+  },
+  // MySQL 配置
+  dbConfig: {
+    database: "myindex",
+    username: "root",
+    password: "114514",
+    host: "localhost",
+    port: 3306,
+  },
+  // 百度翻译配置，自行申请免费版即可（https://fanyi-api.baidu.com/）
+  baiduFanYiConfig: {
+    appid: "811",
+    key: "",
+  },
+};
